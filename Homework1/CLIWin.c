940a55943bf8f127b3a12cb85ccfa2dc953e86b0
dengrenjie <dengrenjie31@gmail.com>
2018-10-06 22:17:43 +0800

    Fix CLIWin problem

--- Homework1/CLIWin.c
+++ Homework1/CLIWin.c
@@ -5,15 +5,21 @@
 int main(void) {
 	char prompt[] = "Type a command: ";
 	char buf[SIZE];
+	DWORD writeLength;
+	DWORD readLength;
+	STARTUPINFO si; // For startup information
+	PROCESS_INFORMATION pi; // For process information
+	DWORD status;
+	HANDLE writeHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE readHandle = GetStdHandle(STD_INPUT_HANDLE);
+
 	while (1) {
 		// Ask the user to type a command:
-		DWORD promptLength = strlen(prompt); //The length of prompt
-		WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), prompt, promptLength, &promptLength, NULL);
+		WriteConsole(writeHandle, prompt, strlen(prompt), &writeLength, NULL);
 		// Read from the standard input the command typed by the user (note 
 		// that fgets also puts into the array buf the ’\n’ character typed 
 		// by the user when the user presses the Enter key on the keyboard):
-		DWORD bufSize = 1024; //The length of buf
-		ReadConsole(GetStdHandle(STD_INPUT_HANDLE), buf, bufSize, &bufSize, NULL);
+		ReadConsole(readHandle, buf, SIZE, &readLength, NULL);
 		// Replace the Enter key typed by the user with ’\0’:
 		for (int i = 0; i < SIZE; i++) {
 			if (buf[i] == '\n' || buf[i] == '\r') {
@@ -24,17 +30,13 @@ int main(void) {
 		// Execute the command typed by the user (only prints it for now):
 		if (strcmp("exit", buf) == 0) // Exit when user type "exit"
 			return 0;
-		STARTUPINFO si; // For startup information
-		PROCESS_INFORMATION pi; // For process information
-		DWORD status;
 
 		ZeroMemory(&si, sizeof(si));
 		si.cb = sizeof(si);
 		ZeroMemory(&pi, sizeof(pi));
 		if (!CreateProcess(NULL, buf, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
 			char errorMsg[] = "CreateProcess failed!\n";
-			DWORD errorMsgLength = strlen(errorMsg);
-			WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), errorMsg, errorMsgLength, &errorMsgLength, NULL);
+			WriteConsole(writeHandle, errorMsg, strlen(errorMsg), &writeLength, NULL);
 			continue;
 		}
 		status = WaitForSingleObject(pi.hProcess, INFINITE);
