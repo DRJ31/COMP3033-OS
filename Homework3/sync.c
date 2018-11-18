a24bd9d0a19773355483df0cac4dd6c5ba62bf79
dengrenjie <dengrenjie31@gmail.com>
2018-11-19 01:08:52 +0800

    Fuck the black magic!

--- Homework3/sync.c
+++ Homework3/sync.c
@@ -11,64 +11,76 @@
 int sum = 0;
 int flag[N] = {0}; // Initialize array for flag
 
-// Check if value is in the array
-// length: Length of range array
-int numInArr(int self, int value, const int range[], int length) {
-    for (int i = 0; i < length; i++) {
-        if (value == range[i] && i != self)
-            return TRUE;
-    }
-    return FALSE;
-}
-
-// Wait for array until it meets condition
-// length: Length of range array
-// start: The first point of flag's range
-// end: The last point of flag's range
-// init: TRUE -> all the elements in flag, FALSE -> any element in flag
-void await(int self, int flag[], int start, int end, const int range[], int length, int init) {
-    int opened = init; // Check if the door has opened
-    if (start > end)
-        return;
-    while (opened == init) {
-        for (int i = start; i <= end; i++) {
-            if (numInArr(self, flag[i], range, length)) {
-                opened = opened ? FALSE : TRUE;
-                break;
-            }
-        }
-    }
-}
-
 // The function executed by each thread.
 void *runner(void *param) {
     int i = *(int *)param; // This thread’s ID number.
     int m;
-    //    int j = i == 1 ? 0 : 1;
-    int openDoor[] = {0, 1, 2}; // Range for waiting open door
-    int closeDoor[] = {4}; // Range for waiting door close
-    int finishExit[] = {0, 1}; // Range for waiting other lower ID to finish exit
-    int waitClose[] = {0, 1, 4}; // Range for ensure every process know door is to be close
+    int DOOR_OPENED = FALSE; // Check if the door is still opening
+    int WANT_EXEC; // Check if any other process want to execute
+    int FINISH_EXIT = FALSE; // Check if lower ID process have done exit
+    int READY_LEAVE = FALSE; // Check if the current process is ready to leave
     
     for(m = 0; m < M; m++) {
         // Pi wants to enter the waiting room:
         flag[i] = 1;
-        await(i, flag, 0, N - 1, openDoor, 3, TRUE); // Atomic test
+
+        // Wait for open door
+        while (!DOOR_OPENED) {
+            DOOR_OPENED = TRUE;
+            for (int j = 0; j < N; j++) {
+                if (flag[j] > 2) {
+                    DOOR_OPENED = FALSE;
+                    break;
+                }
+            }
+        }
+
         // Pi goes through the entrance door
         flag[i] = 3;
         // Check whether other process wants to enter the waiting room
-        if (numInArr(i, 1, flag, N)) {
-            // Then Pi starts waiting inside the waiting room
-            flag[i] = 2;
-            // Until Pj comes inside the waiting room too and the entrance
-            // door closes
-            await(i, flag, 0, N - 1, closeDoor, 1, FALSE); // Wait for a process to enter and close door
+        while (DOOR_OPENED) {
+            int WAIT_ENTER = FALSE; // Check if other process want to enter
+            for (int j = 0; j < N; j++) {
+                if (j == i)
+                    continue;
+                if (flag[j] == 1) {
+                    WAIT_ENTER = TRUE;
+                    break;
+                }
+            }
+            if (WAIT_ENTER) {
+                // Then Pi starts waiting inside the waiting room
+                flag[i] = 2;
+
+                // Wait for a process to enter and close door
+                WANT_EXEC = FALSE;
+                while (!WANT_EXEC) {
+                    for (int j = 0; j < N; j++) {
+                        if (j == i)
+                            continue;
+                        if (flag[j] == 4) {
+                            WANT_EXEC = TRUE;
+                            break;
+                        }
+                    }
+                }
+            }
+            DOOR_OPENED = FALSE;
         }
-        // In all cases, flag[i] becomes 4 (that’s also when the entrance door
-        // automatically closes).
-        flag[i] = 4;
-        await(i, flag, 0, i - 1, finishExit, 2, TRUE); // Wait for all lower ID to finish exit protocol
-        
+
+        flag[i] = 4; // Close the door when no process want to enter
+
+        // Wait for all lower ID to finish exit protocol
+        while (!FINISH_EXIT) {
+            FINISH_EXIT = TRUE;
+            for (int j = 0; j < i; j++) {
+                if (flag[j] > 1) {
+                    FINISH_EXIT = FALSE;
+                    break;
+                }
+            }
+        }
+
         // The Critical Section starts right below
         int s = sum;
         // Even threads increase s, odd threads decrease s.
@@ -84,14 +96,23 @@ void *runner(void *param) {
         delay.tv_sec = 0;
         delay.tv_nsec = 100000000ULL * rand() / RAND_MAX;
         nanosleep(&delay, NULL);
-        
+
         sum = s;
         // The Critical Section ends right above
-        
-        // Exit protocol
-        await(i, flag, i + 1, N - 1, waitClose, 3, TRUE); // Ensure every process know the door is to be close
+
+        // Ensure every process know the door is to be close
+        while (!READY_LEAVE) {
+            READY_LEAVE = TRUE;
+            for (int j = i + 1; j < N; j++) {
+                if (flag[j] == 2 || flag[j] == 3) {
+                    READY_LEAVE = FALSE;
+                    break;
+                }
+            }
+        }
+
         flag[i] = 0; // Leave, reopen door if nobody still in waiting room
-        
+
         // The Remainder Section starts here
         printf("%c", 'A' + i); // Print this thread’s ID number as a letter.
         fflush(stdout);
@@ -103,19 +124,19 @@ int main(void) {
     pthread_t tid[N]; // Thread ID numbers.
     int param[N]; // One parameter for each thread.
     int i;
-    
+
     // Create N threads. Each thread executes the runner function with
     // i as argument.
     for(i = 0; i < N; i++) {
         param[i] = i;
         pthread_create(&tid[i], NULL, runner, &param[i]);
     }
-    
+
     // Wait for N threads to finish.
     for(i = 0; i < N; i++) {
         pthread_join(tid[i], NULL);
     }
-    
+
     printf("\nResult is %d\n", sum);
     return 0;
 }
